9a323efe205837c00560ecb40729661c73270818
SbinSho <suho0232@gmail.com>
2020-11-26 20:54:28 +0900

    gradle project -> springboot project로 변경, gradle version 변경, jUnit Test code 작성 후 passed 완료

---
+++ src/main/java/com/suho/book/springboot/web/HelloContorller.java
@@ -0,0 +1,20 @@
+package com.suho.book.springboot.web;
+
+import com.suho.book.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloContorller {
+
+    @GetMapping("/hello")
+    public String Hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+}
