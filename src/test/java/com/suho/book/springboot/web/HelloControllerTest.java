3d7b2f55259d88bcec97ad89706a034bef8a5f8e
SbinSho <suho0232@gmail.com>
2020-11-27 16:18:03 +0900

    HelloControllerTest 코드에 주석문 추가

--- src/test/java/com/suho/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/suho/book/springboot/web/HelloControllerTest.java
@@ -14,12 +14,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloContorller.class)
+@RunWith(SpringRunner.class) // JUnit에 내장된 실행자 외에 다른 실행자를 실행 시킨다. ( SpringRunner.class )
+@WebMvcTest(controllers = HelloContorller.class) // Web(Spring MVC)에 집중할 수 있는 어노테이션 이다.
 public class HelloControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
+    @Autowired // 빈 자동 주입.
+    private MockMvc mvc; // 웹API를 테스트할 때 사용 한다.
 
     @Test
     public void hello가_리턴된다() throws Exception {
@@ -37,7 +37,8 @@ public class HelloControllerTest {
                 get("/hello/dto")
                         .param("name", name)
                         .param("amount", String.valueOf(amount)))
-                .andExpect(status().isOk())
+                // MockMvc를 통해 /hello 주소로 HTTP GET 요청, 체이닝 지원 여러 검증 기능 이어서 선언 가능.
+                .andExpect(status().isOk()) // 결과 검증
                 .andExpect(jsonPath("$.name", is(name)))
                 .andExpect(jsonPath("$.amount", is(amount)));
 
