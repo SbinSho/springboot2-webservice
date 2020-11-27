1c8d8b7d96ff57d3b397e6dc96f62d370e751397
SbinSho <suho0232@gmail.com>
2020-11-27 16:28:50 +0900

    Spring Data JPA 테스트 코드 작성

---
+++ src/test/java/com/suho/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.suho.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After // Junit에서 단위 테스트가 끝날 때마다 수행되는 메소드를 지정
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("suho0232@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        // assertThat : asserj라는 테스트 검증 라이브러리, 검증하고 싶은 대상을 메소드 인자로 받음.
+        // isEqualTo : asserThat에 있는 값과 isEqualTo의 값을 비교해서 같은 때만 성공.
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+
+
+}
