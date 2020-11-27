fa9e403da89d410f3f4a4e2c7f6306e64e032d0e
SbinSho <suho0232@gmail.com>
2020-11-27 16:26:32 +0900

    실제 DB의 테이블과 매칭될 클래스 Entity 클래스 생성

---
+++ src/main/java/com/suho/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,34 @@
+package com.suho.book.springboot.domain.posts;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter // 선언된 모든 필드의 get 메소드를 생성 ( 롬복의 어노테이션 )
+@NoArgsConstructor // 기본 생성자 자동 추가 (롬복의 어노테이션)
+@Entity // 테이블과 링크될 클래스임을 나타낸다. ( JPA의 어노테이션 )
+public class Posts {
+
+    @Id // 해당 테이블의 PK 필드를 나타낸다.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK의 생성 규칙을 나타낸다.
+    private Long id;
+
+    @Column(length = 500, nullable = false) // 테이블 컬럼의 기본값 외에 추가로 변경이 필요한 옵션이 있으면 사용한다.
+    private  String title;
+
+    @Column(columnDefinition = "TEXT", nullable =false)
+    private String content;
+
+    private String author;
+
+    @Builder // 해당 클래스의 빌더 패턴 클래스를 생성, 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
