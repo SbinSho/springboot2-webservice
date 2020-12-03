bb382e5a49bdcad6ed0729ad3361a0702b1fbb94
SbinSho <suho0232@gmail.com>
2020-12-03 20:23:38 +0900

    IndexContorller 작성, IndexContorllerTest 작성

---
+++ src/main/java/com/suho/book/springboot/web/IndexContorller.java
@@ -0,0 +1,14 @@
+package com.suho.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexContorller {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
